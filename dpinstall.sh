efa129023b247f0dabab3ac21771bffa18c58617
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2024-04-30 06:44:58 -0700

    Add files via upload

---
+++ dpinstall.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+Installing dependencies...
+
+Checking for Homebrew...
+# Check for Homebrew
+if test ! $(which brew); then
+    echo "no. Installing..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "yes."
+fi
+
+Checking for node...
+# Check for Node.js and npm
+if test ! $(which node); then
+    echo "no. Installing..."
+    brew install node
+else
+    echo "yes"
+fi
+
+Checking for node...
+if test ! $(which npm); then
+    echo "no. Installing..."
+    brew install npm
+else
+    echo "yes."
+fi
+
+Checking for npx...
+# Check for npx
+if test ! $(which npx); then
+    echo "no. Installing..."
+    npm install -g npx
+else
+    echo "yes"
+fi
