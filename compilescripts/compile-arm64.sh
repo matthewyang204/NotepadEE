62212ba0001dc28ee69d39566eae3fa06b24fb5b
Matthew Yang <matthewyang@Matthew-Yangs-MacBook-Pro.local>
2024-08-04 12:41:23 -0700

    Create compiling scripts

---
+++ compilescripts/compile-arm64.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Compiling Notepad=="
+echo "Compiling main first..."
+pyinstaller --windowed -i Notepad.png --hidden-import=tkinter Notepad==.py
+echo "done
+
+echo "Caching compiled main..."
+mv dist/Notepad==.app ~/Downloads/
+rm -rf dist/*
+echo "done"
+
+echo "Now compiling clone..."
+pyinstaller --windowed -i Notepad.png --hidden-import=tkinter Clone/Notepad==.py
+echo "done"
+
+echo "Assembling binary..."
+mkdir ~/Downloads/Notepad==.app/Contents/Resources/Clone
+mv dist/* ~/Downloads/Notepad==.app/Contents/Resources/Clone/
+echo "done"
+
+echo "Packaging app..."
+cd ~/Downloads
+7z a -tzip Notepad==.zip Notepad==.app
+echo "done"
