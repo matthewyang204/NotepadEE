efb73b8bd1e7f88bddfadac5c667611dc0b256dd
Matthew Yang <7930663-matthewyang20@users.noreply.replit.com>
2024-10-18 19:26:01 +0000

    Add command line arguments to Linux version

--- Linux/src/Notepad==.py
+++ Linux/src/Notepad==.py
@@ -3,24 +3,39 @@ from tkinter import filedialog
 import os
 from tkinter import messagebox
 from tkinter import font
+import sys
+
+filearg = sys.argv
+if len(filearg) <= 1:
+    openFile = 0
+    print(
+        "No arguments provided. Proceeding to load program with last known file..."
+    )
+    print("Program loaded")
+else:
+    openFile = 1
+    print("Assuming argument is the file to open. Loading file...")
+    fileToBeOpened = filearg[1]
 
 global file_open
-file_open=0
-last_file_path  = os.path.join(os.path.expanduser('~'),  '.notepadee', 'cache', 'last_file_path')
+file_open = 0
+last_file_path = os.path.join(os.path.expanduser('~'), '.notepadee', 'cache',
+                              'last_file_path')
 if os.path.exists(last_file_path):
     with open(last_file_path, 'r') as file:
-        current_file  = file.read()
+        current_file = file.read()
         if current_file.strip() == '':  # Check if the file is empty
             file_open = 0
         else:
             file_open = 1
-    
-else:
-    current_file  =  ""
-    file_open=0
 
-last_write=os.path.join(os.path.expanduser('~'),  '.notepadee', 'cache', 'last_write')
-folder_path  = os.path.join(os.path.expanduser('~'),  '.notepadee', 'cache')
+else:
+    current_file = ""
+    file_open = 0
+
+last_write = os.path.join(os.path.expanduser('~'), '.notepadee', 'cache',
+                          'last_write')
+folder_path = os.path.join(os.path.expanduser('~'), '.notepadee', 'cache')
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
@@ -45,6 +60,7 @@ word_count_var = tk.StringVar()
 word_count_label = tk.Label(status_frame, textvariable=word_count_var)
 word_count_label.pack(side=tk.LEFT)
 
+
 def get_font_for_platform():
     if os.name == 'nt':
         return font.Font(family="Consolas", size=12)
@@ -53,48 +69,56 @@ def get_font_for_platform():
     else:
         return font.Font(family="DejaVu Sans Mono", size=12)
 
+
 text_font = get_font_for_platform()
 text_area = tk.Text(root, width=100, height=80, wrap=tk.WORD, undo=True)
 text_area.config(font=text_font)
 
+
 def debug_var(event=None):
-#    global file_open, current_file
-#    if current_file:
-#        print("Current file variable works")
-#        print(current_file)
-#    else:
-#        print("Not intact")
-#    if file_open:
-#        print("File_open variable is intact")
-#        print(file_open)
-#    else:
-#        print("Not working")
+    #    global file_open, current_file
+    #    if current_file:
+    #        print("Current file variable works")
+    #        print(current_file)
+    #    else:
+    #        print("Not intact")
+    #    if file_open:
+    #        print("File_open variable is intact")
+    #        print(file_open)
+    #    else:
+    #        print("Not working")
     return 'break'
+
 
 def autosave_file(event=None):
     global current_file
     global file_open
     try:
-        if file_open==1:
+        if file_open == 1:
             with open(current_file, 'w') as file:
                 text = text_area.get('1.0', 'end-1c')
                 file.write(text)
     except FileNotFoundError:
         return 'break'
 
+
 def write_cache(event=None):
     global current_file, file_open
-    with open(os.path.join(os.path.expanduser('~'), '.notepadee', 'cache', 'last_write'), 'w') as file:
+    with open(
+            os.path.join(os.path.expanduser('~'), '.notepadee', 'cache',
+                         'last_write'), 'w') as file:
         file.write(text_area.get('1.0', 'end-1c'))
-    last_file_path = os.path.join(os.path.expanduser('~'), '.notepadee', 'cache', 'last_file_path')
+    last_file_path = os.path.join(os.path.expanduser('~'), '.notepadee',
+                                  'cache', 'last_file_path')
     with open(last_file_path, 'w') as file:
         file.write(current_file)
     autosave_file()
 
+
 def save_as(event=None):
     global current_file, file_open
     file_path = filedialog.asksaveasfilename(defaultextension=".txt")
-    current_file=file_path
+    current_file = file_path
     if not file_path:
         return 'break'
     try:
@@ -102,24 +126,26 @@ def save_as(event=None):
             text = text_area.get(1.0, "end-1c")
             file.write(text)
         write_cache()
-        file_open=1
+        file_open = 1
     except FileNotFoundError:
         messagebox.showerror("Error", "File not found.")
+
 
 def open_file(event=None):
     global current_file, file_open
     file_path = filedialog.askopenfilename(filetypes=[("All Files", "*.*")])
     if file_path:
         text_area.delete(1.0, "end")
-        current_file=file_path
+        current_file = file_path
         with open(file_path, 'r') as file:
             text_area.insert(1.0, file.read())
-        file_open=1
+        file_open = 1
     write_cache()
+
 
 def save_file(event=None):
     global current_file, file_open
-    if file_open==1:
+    if file_open == 1:
         try:
             debug_var()
             with open(current_file, 'w') as file:
@@ -129,16 +155,20 @@ def save_file(event=None):
         except FileNotFoundError:
             return 'break'
     else:
-        response = messagebox.askyesno("Create new file", "The file does not exist. Do you want to create it as a new file?")
+        response = messagebox.askyesno(
+            "Create new file",
+            "The file does not exist. Do you want to create it as a new file?")
         if response:
             save_as()
+
 
 def new_file(event=None):
     global current_file, file_open
     text_area.delete(1.0, "end")
-    current_file=""
+    current_file = ""
     write_cache()
-    file_open=0
+    file_open = 0
+
 
 def cut_text(event=None):
     text_area.clipboard_clear()
@@ -146,18 +176,22 @@ def cut_text(event=None):
     text_area.delete("sel.first", "sel.last")
     return 'break'
 
+
 def copy_text(event=None):
     text_area.clipboard_clear()
     text_area.clipboard_append(text_area.get("sel.first", "sel.last"))
     return 'break'
 
+
 def paste_text(event=None):
     text_area.insert("insert", text_area.clipboard_get())
     return 'break'
 
+
 def select_all_text(event=None):
     text_area.tag_add("sel", "1.0", "end")
     return 'break'
+
 
 def undo(event=None):
     try:
@@ -165,11 +199,13 @@ def undo(event=None):
     except tk.TclError:
         pass
 
+
 def redo(event=None):
     try:
         text_area.edit_redo()
     except tk.TclError:
         pass
+
 
 def find_and_replace():
     popup = tk.Toplevel(root)
@@ -180,7 +216,8 @@ def find_and_replace():
     find_entry = tk.Entry(popup)
     find_entry.pack()
 
-    replace_label = tk.Label(popup, text="Enter the text you want to it replace with:")
+    replace_label = tk.Label(
+        popup, text="Enter the text you want to it replace with:")
     replace_label.pack()
     replace_entry = tk.Entry(popup)
     replace_entry.pack()
@@ -200,6 +237,7 @@ def find_and_replace():
     replace_button = tk.Button(popup, text="Replace", command=perform_replace)
     replace_button.pack()
 
+
 def update_line_number(event=None):
     line, column = text_area.index(tk.INSERT).split('.')
     line_var.set("Line: " + line)
@@ -207,17 +245,32 @@ def update_line_number(event=None):
     words = text_area.get(1.0, 'end-1c').split()
     word_count_var.set("Words: " + str(len(words)))
 
+
 def increase_font_size(event=None):
     current_size = text_font['size']
     text_font.config(size=current_size + 1)
+
 
 def decrease_font_size(event=None):
     current_size = text_font['size']
     text_font.config(size=current_size - 1)
 
+
 def runonkeyrelease(event=None):
     write_cache()
     update_line_number()
+
+
+if openFile == 1:
+    file_path = fileToBeOpened
+    if file_path:
+        text_area.delete(1.0, "end")
+        current_file = file_path
+        with open(file_path, 'r') as file:
+            text_area.insert(1.0, file.read())
+        file_open = 1
+    write_cache()
+    print("File loaded")
 
 text_area.pack(fill=tk.BOTH, expand=tk.YES)
 text_area.bind('<KeyRelease>', runonkeyrelease)
