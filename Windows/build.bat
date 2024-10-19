1b42f3e6431fcefa65f97ec7240ce054bf66bdb5
Matthew Yang <matthew.yang204@gmail.com>
2024-10-19 10:39:21 -0700

    Update build.bat

--- Windows/build.bat
+++ Windows/build.bat
@@ -1,4 +1,4 @@
 nuitka --standalone --enable-plugin=tk-inter --output-dir=dist src/Notepad==.py
-xcopy /E /I "dist/Notepad==.dist" "dist/Notepad=="
-copy Notepad.png dist/Notepad==/
+xcopy /E /I "dist\Notepad==.dist" "dist\Notepad=="
+copy Notepad.ico dist\Notepad==\
 echo "Build completed. To install, compile the Inno Setup installer and then run it."
