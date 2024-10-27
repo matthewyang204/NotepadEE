2938143ecb3763fd191f03422e59a0ff316d0f0b
Matthew Yang <matthew.yang204@gmail.com>
2024-10-27 16:02:48 -0700

    Update build system to PyInstaller due to Nuitka binaries being constantly flagged as "virus" (false positive)

--- Windows/build.bat
+++ Windows/build.bat
@@ -1,1 +1,1 @@
-nuitka --standalone --enable-plugin=tk-inter --windows-disable-console --output-dir=dist src\Notepad==.py
+pyinstaller --hidden-import=tkinter -i Notepad.ico src\Notepad==.py
--- Windows/postbuild.bat
+++
@@ -1,3 +0,0 @@
-xcopy /E /I "dist\Notepad==.dist" "dist\Notepad=="
-rcedit dist\Notepad==\Notepad==.exe --set-icon Notepad.ico
-echo "Build completed. To install, compile the Inno Setup installer and then run it."
