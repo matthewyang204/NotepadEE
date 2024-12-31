6557af6c4464bb747a6e0b5e39234da09ebf5722
Matthew Yang <matthew.yang204@gmail.com>
2024-12-31 13:12:00 -0800

    python can't find module

--- Windows/build-debug.bat
+++ Windows/build-debug.bat
@@ -1,2 +1,2 @@
 @echo off
-python -m pyinstaller --hide-console hide-early --hidden-import=tkinter -i Notepad.ico src\Notepad==.py
+pyinstaller --hide-console hide-early --hidden-import=tkinter -i Notepad.ico src\Notepad==.py
--- Windows/build.bat
+++ Windows/build.bat
@@ -1,2 +1,2 @@
 @echo off
-python -m pyinstaller --noconsole --hidden-import=tkinter -i Notepad.ico src\Notepad==.py
+pyinstaller --noconsole --hidden-import=tkinter -i Notepad.ico src\Notepad==.py
