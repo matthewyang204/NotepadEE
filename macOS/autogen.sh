5b57c4ca39f2c7bad1be72adb57e1d0ecc9293b5
Matthew Yang <matthew.yang204@gmail.com>
2025-01-01 21:39:43 -0800

    Remove pyinstaller build target, as it works best with nuitka

--- macOS/autogen.sh
+++ macOS/autogen.sh
@@ -57,22 +57,7 @@ echo "Configuring build..."
 # Check if the configure command was successful
 if [ $? -eq 0 ]; then
     echo "Configuration successful. Proceeding to build..."
-    
-    # Build
-    echo "Building..."
-    echo "Please select a compiler:"
-    echo "1. 'pyinstaller' (recommended, can only build host architecture)"
-    echo "2. 'nuitka' (not recommended, can cross compile)"
-    echo "Enter the name of the compiler you want to use:"
-    read COMPILER
-    if [ "$COMPILER" = "pyinstaller" ]; then
-        make build-pyinstaller
-    elif [ "$COMPILER" = "nuitka" ]; then
-        make build-nuitka
-    else
-        echo "'$COMPILER' is not a valid option. Please select a proper compiler. Exiting..."
-        exit 1
-    fi
+    make
     echo "done"
 else
     echo "Configuration failed. Exiting."
