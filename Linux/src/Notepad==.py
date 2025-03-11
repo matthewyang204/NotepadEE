1bfbff3b45a8b0d6b8380b9c844df1f31ef85df3
Matthew Yang <matthew.yang204@gmail.com>
2025-03-11 10:12:52 -0700

    Bump version number correctly

--- Linux/src/Notepad==.py
+++ Linux/src/Notepad==.py
@@ -22,7 +22,7 @@ def printlog(message):
         file.write(str(message) + '\n')
     print(message)
 
-versionInfo = """Notepad==, version 4.9.3
+versionInfo = """Notepad==, version 5.0.0
 (C) 2024-2025, Matthew Yang"""
 
 helpInfo = f"""{versionInfo}
--- Windows/src/Notepad==.py
+++ Windows/src/Notepad==.py
@@ -3,7 +3,7 @@ from tkinter import messagebox, font, filedialog
 import os
 import sys
 
-versionInfo = """Notepad==, version 4.9.3
+versionInfo = """Notepad==, version 5.0.0
 (C) 2024-2025, Matthew Yang"""
 
 helpInfo = f"""{versionInfo}
