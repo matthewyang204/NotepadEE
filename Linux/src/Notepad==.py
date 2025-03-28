a978b5d8307e3f573fd7df7686d5e2694297c8e4
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2025-03-28 19:31:26 +0000

    Bump version number

--- Linux/src/Notepad==.py
+++ Linux/src/Notepad==.py
@@ -22,7 +22,7 @@ def printlog(message):
         file.write(str(message) + '\n')
     print(message)
 
-versionInfo = """Notepad==, version 5.0.5
+versionInfo = """Notepad==, version 5.0.6
 (C) 2024-2025, Matthew Yang"""
 
 helpInfo = f"""{versionInfo}
