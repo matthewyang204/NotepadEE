79b651a0ad23b76f9b33da3dd9aeb8f7a059c419
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2025-05-06 18:04:49 +0000

    As there is no newWindow function in Windows, use the old new_file function

--- Windows/src/Notepad==.py
+++ Windows/src/Notepad==.py
@@ -885,15 +885,15 @@ root.config(menu=menu)
 
 file_menu = tk.Menu(menu)
 menu.add_cascade(label="File", menu=file_menu)
-# file_menu.add_command(label="New", command=new_file)
+file_menu.add_command(label="New", command=new_file)
 # if platform.system() == "Darwin":
     # file_menu.add_command(label="New Window", command=newWindow_macOS)
 # elif platform.system() == "Linux":
     # file_menu.add_command(label="New Window", command=newWindow_Linux)
-if platform.system() == "Darwin":
-    file_menu.add_command(label="New", command=newWindow)
-elif platform.system() == "Linux":
-    file_menu.add_command(label="New", command=newWindow)
+# if platform.system() == "Darwin":
+#     file_menu.add_command(label="New", command=newWindow)
+# elif platform.system() == "Linux":
+#     file_menu.add_command(label="New", command=newWindow)
 file_menu.add_command(label="Open...", command=open_file_v2)
 file_menu.add_command(label="Save", command=save_file2)
 file_menu.add_command(label="Save as...", command=save_as)
