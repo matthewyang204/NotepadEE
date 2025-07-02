feb9df1584f538e99fc09bc2af92bc352c64a9cb
Matthew Yang <matthew.yang204@gmail.com>
2025-07-01 18:58:30 -0700

    Fix up the logging issues

--- Windows/src/Notepad==.py
+++ Windows/src/Notepad==.py
@@ -86,7 +86,7 @@ def printlog(message):
         file.write("Notepad== at " + str(pid) + ": " + str(message))
     print("Notepad== at " + str(pid) + ": " + str(message))
 
-versionInfo = """Notepad==, version 5.2.0
+versionInfo = """Notepad==, version 5.1.0
 (C) 2024-2025 Matthew Yang"""
 
 helpInfo = versionInfo + """
