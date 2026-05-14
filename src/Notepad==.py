ee8d25a55c1ad0cc474bc74623781cfb8c0433d1
Matthew Yang <matthew.yang204@gmail.com>
2026-05-14 06:41:53 -0700

    Only do this when building on platforms that use pyinstaller

--- src/Notepad==.py
+++ src/Notepad==.py
@@ -1,12 +1,14 @@
 import main
+import platform
 
 # To trigger PyInstaller
-import pyperclip
-import tkinter
-import tklinenums
-from spellchecker import SpellChecker
-import idlelib.colorizer
-import idlelib.percolator
+if platform.system() != "Darwin":
+    import pyperclip
+    import tkinter
+    import tklinenums
+    from spellchecker import SpellChecker
+    import idlelib.colorizer
+    import idlelib.percolator
 
 if __name__ == "__main__":
     main.main()
