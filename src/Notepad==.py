18472b79a0153cd437b4a7cfdc553b8b5fe9aad3
Matthew Yang <matthew.yang204@gmail.com>
2026-05-13 21:14:50 -0700

    Just bundle all this shit as src and then trigger pyinstaller's imports in the compiled launcher file

--- src/Notepad==.py
+++ src/Notepad==.py
@@ -1,4 +1,12 @@
 import main
 
+# To trigger PyInstaller
+import pyperclip
+import tkinter
+import tklinenums
+from spellchecker import SpellChecker
+import idlelib.colorizer
+import idlelib.percolator
+
 if __name__ == "__main__":
     main.main()
