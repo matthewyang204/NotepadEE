d8fccb882f5a29fa851c59dd3e235e8fccf51c98
Matthew Yang <matthew.yang204@gmail.com>
2026-05-14 06:42:32 -0700

    Add more imports to force include

--- src/Notepad==.py
+++ src/Notepad==.py
@@ -3,12 +3,31 @@ import platform
 
 # To trigger PyInstaller
 if platform.system() != "Darwin":
+    import os
+    import sys
+    import hashlib
+    import builtins
+    import tempfile
+    import re
+    import pathlib
+    import traceback
+    import errno
+    import platform
+    import subprocess
+    import threading
+    import signal
+    import shutil
+    from pathlib import Path
+    import tkinter as tk
+    from tkinter import messagebox
+    from tkinter import filedialog
+    from tkinter import font
+    import idlelib.colorizer
+    import idlelib.percolator
     import pyperclip
     import tkinter
     import tklinenums
     from spellchecker import SpellChecker
-    import idlelib.colorizer
-    import idlelib.percolator
 
 if __name__ == "__main__":
     main.main()
